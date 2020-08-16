140e51a8c8c2a3f3809fbc6bf937c3ceca828bce
Cameron Williams <cameronrw.nz@gmail.com>
2020-08-16 21:17:12 +0700

    Update Homepage

---
+++ precache-manifest.8016ff2fdb98eebb795b1229ce7510e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbb012848c0f2db5297dc39398df507",
+    "url": "/crusade-log/index.html"
+  },
+  {
+    "revision": "eb9805794e86ee23edf9",
+    "url": "/crusade-log/static/css/main.c59dcd01.chunk.css"
+  },
+  {
+    "revision": "df54bb9ec8ff6201a2bb",
+    "url": "/crusade-log/static/js/2.4d15a386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crusade-log/static/js/2.4d15a386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb9805794e86ee23edf9",
+    "url": "/crusade-log/static/js/main.e460d457.chunk.js"
+  },
+  {
+    "revision": "ad56156e9493334dbb9e",
+    "url": "/crusade-log/static/js/runtime-main.8aa0809d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71fcf867504c7168e25a9003e5d819e2.js"
+  "/crusade-log/precache-manifest.8016ff2fdb98eebb795b1229ce7510e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crusade-log/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
